42cdc27c2e1b00cf4274cc15de7d664c73b46b3f
joshua girma debele <joshdebele8@gmail.com>
2024-01-13 13:05:42 -0700

    Portfolio setup

---
+++ src/app/components/HeroSection.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <h1 className="text-white mb-4 text-6xl">Hello, I'm Joshua Debele</h1>
+      <p className="text-[#AB87BE]"></p>
+    </section>
+  );
+};
+
+export default HeroSection;
