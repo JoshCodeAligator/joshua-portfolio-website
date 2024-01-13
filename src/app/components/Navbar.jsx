222cb3b551386c12bdcbaa510fbb9216f633a0d6
joshua girma debele <joshdebele8@gmail.com>
2024-01-13 16:22:34 -0700

    Implement React methods

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -22,7 +22,7 @@ const Navbar = () => {
   const [navbarOpen, setNavbarOpen] = useState(false);
   return (
     <nav className="fixed top-0 left-0 right-0 z-10 bg-[#121212] bg-opacity-90">
-      <div className="flex flex-wrap items-center justify-between mx-auto px-4">
+      <div className="flex flex-wrap items-center justify-between mx-auto px-4 py-2">
         <Link
           href={"/"}
           className="text-2xl md:text-5xl text-white font-semibold"
@@ -30,12 +30,14 @@ const Navbar = () => {
           LOGO
         </Link>
         <div className="mobile-menu block md:hidden">
-          {navbarOpen ? (
-            <button className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200">
+          {!navbarOpen ? (
+            <button onClick={() => setNavbarOpen(true)} className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white">
               <Bars3Icon className="h-5 w-5" />
             </button>
           ) : (
-            <button></button>
+            <button onClick={() => setNavbarOpen(false)} className="flex items-center px-3 py-2 border rounded border-slate-200 text-slate-200 hover:text-white hover:border-white">
+              <XmarkIcon className="h-5 w-5" />
+            </button>
           )}
         </div>
         <div className="menu hidden md:block md:w-auto" id="navbar">
