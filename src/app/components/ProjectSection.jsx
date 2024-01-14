4aab82c3d4c79eb1552f37b770b73df85d974d8c
joshua girma debele <joshdebele8@gmail.com>
2024-01-13 21:02:28 -0700

    Mapping through ProjectData

--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -1,11 +1,56 @@
 import React from "react";
 import ProjectCard from "./ProjectCard";
 
+const projectsData = [
+    {
+        id: 1,
+        title: "React Portfolio Website",
+        description: "Project 1 description",
+        image: "/images/projects/1.png",
+        tag: ["All", "Web"],
+    },
+    {
+        id: 2,
+        title: "React Portfolio Website",
+        description: "Project 1 description",
+        image: "/images/projects/1.png",
+        tag: ["All", "Web"],
+    },
+    {
+        id: 3,
+        title: "React Portfolio Website",
+        description: "Project 1 description",
+        image: "/images/projects/1.png",
+        tag: ["All", "Web"],
+    },
+    {
+        id: 4,
+        title: "React Portfolio Website",
+        description: "Project 1 description",
+        image: "/images/projects/1.png",
+        tag: ["All", "Web"],
+    },
+    {
+        id: 5,
+        title: "React Portfolio Website",
+        description: "Project 1 description",
+        image: "/images/projects/1.png",
+        tag: ["All", "Web"],
+    },
+    {
+        id: 6,
+        title: "React Portfolio Website",
+        description: "Project 1 description",
+        image: "/images/projects/1.png",
+        tag: ["All", "Web"],
+    },
+]
+
 const ProjectSection = () => {
   return (
     <>
       <h2>My Projects</h2>
-      <div></div>
+      <div>{projectsData.map((project) => <ProjectCard key={project.id} title={project.title} description={project.description} imgUrl={project.image}/>)}</div>
     </>
   );
 };
