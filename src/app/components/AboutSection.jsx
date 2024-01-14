dd5b2fee7fa838c1f59b49a382c300436741c797
joshua girma debele <joshdebele8@gmail.com>
2024-01-13 20:43:20 -0700

    Set Active Tab

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -1,18 +1,48 @@
-import React from "react";
+"use client";
+import React, { useTransition, useState } from "react";
 import Image from "next/image";
+import TabButton from "./TabButton";
 
 const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
   return (
     <section className="text-white">
       <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
         <Image src="/images/about-image.png" width={500} height={500} />
         <div>
           <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
-          <p className="text-base lg:text-lg">I am a full stack developer add more things later.</p>
+          <p className="text-base lg:text-lg">
+            I am a full stack developer add more things later.
+          </p>
           <div className="flex flex-row mt-8">
-            <span>Skills</span>
-            <span>Education</span>
-            <span>Experience</span>
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab == "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab == "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab == "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
           </div>
         </div>
       </div>
