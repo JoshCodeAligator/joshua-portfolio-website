b1f497a56f6d1d6621d6e804f61747e8f65a70cc
joshua girma debele <joshdebele8@gmail.com>
2024-01-13 21:48:58 -0700

    ProjectTag Setup

--- src/app/components/ProjectSection.jsx
+++ src/app/components/ProjectSection.jsx
@@ -8,6 +8,8 @@ const projectsData = [
     description: "Project 1 description",
     image: "/images/projects/1.png",
     tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
   {
     id: 2,
@@ -15,6 +17,8 @@ const projectsData = [
     description: "Project 1 description",
     image: "/images/projects/1.png",
     tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
   {
     id: 3,
@@ -22,6 +26,8 @@ const projectsData = [
     description: "Project 1 description",
     image: "/images/projects/1.png",
     tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
   {
     id: 4,
@@ -29,6 +35,8 @@ const projectsData = [
     description: "Project 1 description",
     image: "/images/projects/1.png",
     tag: ["All", "Web"],
+    gitUrl: "/",
+    previewUrl: "/",
   },
   {
     id: 5,
@@ -36,21 +44,30 @@ const projectsData = [
     description: "Project 1 description",
     image: "/images/projects/1.png",
     tag: ["All", "Web"],
-  }
+    gitUrl: "/",
+    previewUrl: "/",
+  },
 ];
 
 const ProjectSection = () => {
   return (
     <>
-      <h2 className="text-center text-4xl font-bold text-white mt-4">My Projects</h2>
-      <div>
+      <h2 className="text-center text-4xl font-bold text-white mt-4">
+        My Projects
+      </h2>
+      <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
+        <button className="rounded-full border-2 border-purple-500 px-6 py-3 text-xl cursor-pointer">All</button>
+        <button className="rounded-full border-2 border-slate-600 hover:border-white px-6 py-3 text-xl cursor-pointer">Web</button>
+      </div>
+      <div className="grid md:grid-cols-3 gap-8 mg:gap-12">
         {projectsData.map((project) => (
           <ProjectCard
             key={project.id}
             title={project.title}
             description={project.description}
             imgUrl={project.image}
-            tags={project}
+            gitUrl={project.gitUrl}
+            previewUrl={project.previewUrl}
           />
         ))}
       </div>
