eb63431cb8a2d12de4c3a9767978fc23cf839153
joshua girma debele <joshdebele8@gmail.com>
2024-01-13 21:15:38 -0700

    Implement ProjectSection

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -4,12 +4,14 @@ const ProjectCard = ({ imgUrl, title, description }) => {
   return (
     <div>
       <div
-        className="h-52 md:h-72"
+        className="h-52 md:h-72 rounded-t-xl relative group"
         style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
-      ></div>
-      <div className="text-white">
-        <h5>{title}</h5>
-        <p>{description}</p>
+      >
+        <div className="overlay absolute top-0 left-0 w-full h-full bg-[#181818] bg-opacity-0 hidden"></div>
+      </div>
+      <div className="text-white rounded-b-xl bg-[#181818] py-6 px-4">
+        <h5 className="text-xl font-semibold mb-2">{title}</h5>
+        <p className="text-[#ADB7BE]">{description}</p>
       </div>
     </div>
   );
