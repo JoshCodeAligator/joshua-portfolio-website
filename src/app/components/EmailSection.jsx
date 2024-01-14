9acbd0ab70af96dd722614f9a4d4d76101770a3f
joshua girma debele <joshdebele8@gmail.com>
2024-01-13 23:16:20 -0700

    Handling Requests with JSON data

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -6,8 +6,27 @@ import Link from "next/link";
 import Image from "next/image";
 
 const EmailSection = () => {
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = {
+            email: e.target.email.value,
+            subject: e.target.subject.value,
+            message: e.target.message.value,
+        }
 
- 
+        const JSONdata = JSON.stringify(data);
+        const endpoint = "/api/send"
+        
+        const options = {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json',
+            },
+            body: JSONdata
+        }
+
+        const response = await fetch(endpoint, options);
+    }
   return (
     <section className="grid md:grid-cols-2 my-12 md:my-12 py-24 gap-4 relative">
       <div className="bg-[radial-gradient(ellipse_at_center,_var(--tw-gradient-stops))] from-purple-900 to-transparent rounded-full h-80 w-80 z-0 blur-lg absolute top-3/4 -left-4 transform -translate-x-1/2 -translate-1/2"></div>
